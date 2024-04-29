bd8a3dd450076c829e475be1adf8607f073798a9
FanniFarkas4 <f.fanni0202+iet@gmail.com>
2024-04-29 11:05:02 +0200

    Not reuse Random problem solved

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,8 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  private Random generator = new Random();
+
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
@@ -34,9 +36,7 @@ public class TorpedoStore {
     }
 
     boolean success = false;
-
-    // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+  
     //komment1
     //komment2
     double r = generator.nextDouble();
